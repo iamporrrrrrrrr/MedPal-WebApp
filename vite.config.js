eb5bf65db49f71202451985a1b4288f867981d46
Theeraphat <theeraphat.tr@gmail.com>
2024-11-02 22:20:43 +0800

    Edit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // Clear the `dist` folder on every build
+  },
   plugins: [react()],
   base: '/MedPal-WebApp/',
 })
